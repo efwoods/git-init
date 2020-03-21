1a8594a3874ef1459719076992c9f673dad9dd3e
efwoods <efwoods@g.clemson.edu>
2020-03-21 14:35:57 -0700

    bug fix

--- git-repo.sh
+++ git-repo.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Create a new git repo
 # $1 repo name
 
@@ -9,16 +9,14 @@ USERNAME=""
 TOKEN=""
 
 main(){
-    while IFS=$'\n' read -a line; 
-    do
-        config+=(${line})
-    done < $CONFIGFILE
+    config=()
+    while IFS=$'\n' read -a line; do config+=(${line}); done < $CONFIGFILE;
     USEREMAIL=${config[0]}
     USERNAME=${config[1]}
     read TOKEN < TOKENFILE
 
-    if USEREMAIL == "" || USERNAME == "" || USEREMAIL == "#git-user.email" || USERNAME == "#git-user.name"  || TOKEN == "" || TOKEN == "#gitauthtoken"
-    do
+    if [USEREMAIL == "" || USERNAME == "" || USEREMAIL == "#git-user.email" || USERNAME == "#git-user.name"  || TOKEN == "" || TOKEN == "#gitauthtoken"]
+    then
         echo "Set git user.email config"
         read USEREMAIL
         echo USEREMAIL > .config
@@ -26,24 +24,29 @@ main(){
         echo "Set git user.name config"
         read USERNAME
         echo USERNAME >> .config
-        configure()
+
         echo "set git Auth Token"
         read TOKEN
         echo TOKEN > .gitauthtoken
-    done
 
-    if USEREMAIL == "" || USERNAME == "" || USEREMAIL == "#git-user.email" || USERNAME == "#git-user.name" || TOKEN == "" || TOKEN == "#gitauthtoken"
-        do
-            echo "error setting config"
-        done
+        config=()
+        while IFS=$'\n' read -a line; do config+=(${line}); done < $CONFIGFILE;
+        USEREMAIL=${config[0]}
+        USERNAME=${config[1]}
+        read TOKEN < TOKENFILE
+    fi
+
+    if [USEREMAIL == "" || USERNAME == "" || USEREMAIL == "#git-user.email" || USERNAME == "#git-user.name" || TOKEN == "" || TOKEN == "#gitauthtoken"]
+    then
+        echo "error setting config"
     else
         echo "Git user.email set to: $USEREMAIL"
         echo "Git user.name set to: $USERNAME"
         echo "Git Auth Token set"
 
-        REPONAME = $1
+        REPONAME=$1
         echo "REPONAME: is $1"
-        mkdir $HOME/REPONAME && cd $HOME/REPONAME
+        mkdir $HOME/$REPONAME && cd $HOME/$REPONAME
         echo "created repo and moved to repo"
         git init
         echo "initialized git repository"
@@ -54,7 +57,7 @@ main(){
 
         git config --user.email USEREMAIL
         git config --user.name USERNAME
-        DATASTRING = "'{\"name\":\"$REPONAME\",\"homepage\":\"https://github.com\",\"private\": false,\"has_issues\":true,\"has_projects\":true,\"has_wiki\":true}'"
+        DATASTRING="'{\"name\":\"$REPONAME\",\"homepage\":\"https://github.com\",\"private\": false,\"has_issues\":true,\"has_projects\":true,\"has_wiki\":true}'"
         curl -H "Authorization: token $TOKEN" --request POST --data $DATASTRING https://api.github.com/user/repos
         ORIGIN="https://github.com/$USERNAME/$REPONAME.git"
         git remote add origin $ORIGIN
@@ -63,10 +66,13 @@ main(){
         git commit -m 'init'
         git push -u orign master
         echo "repo created at $HOME/$REPONAME"
+    fi
 }
 
-if $1 == ""
+if [$1 == ""]
+then
     echo "ERROR enter repo name"
     echo "script syntax: ./git-repo.sh [REPONAME]"
 else
-    main() $1
+    main $1
+fi
